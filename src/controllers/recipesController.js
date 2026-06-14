358d9cc89bfbf5b53156618df30dbfad4fc0328e
YuriyStepanishin <y.stepanishin@gmail.com>
2026-06-14 14:36:54 +0300

    fix recipesController

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -1,32 +1,32 @@
-import { getAllRecipes, getOwnRecipes } from '../services/recipesService.js';
+// import { getAllRecipes, getOwnRecipes } from '../services/recipesService.js';
 
-export const getAllRecipesController = async (req, res, next) => {
-  try {
-    const result = await getAllRecipes(req.query);
+// export const getAllRecipesController = async (req, res, next) => {
+//   try {
+//     const result = await getAllRecipes(req.query);
 
-    res.status(200).json({
-      status: 200,
-      message: 'Recipes fetched successfully',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+//     res.status(200).json({
+//       status: 200,
+//       message: 'Recipes fetched successfully',
+//       data: result,
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-export const getOwnRecipesController = async (req, res, next) => {
-  try {
-    const result = await getOwnRecipes({
-      owner: req.user._id,
-      ...req.query,
-    });
+// export const getOwnRecipesController = async (req, res, next) => {
+//   try {
+//     const result = await getOwnRecipes({
+//       owner: req.user._id,
+//       ...req.query,
+//     });
 
-    res.status(200).json({
-      status: 200,
-      message: 'Own recipes fetched successfully',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+//     res.status(200).json({
+//       status: 200,
+//       message: 'Own recipes fetched successfully',
+//       data: result,
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
