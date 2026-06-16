196b89d0cab540592cb90a3d072b5f5d5e83959d
Bodya Janchenko <robotosoid2000@gmail.com>
2026-06-16 11:33:25 +0300

    delete swager comments

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,28 +4,6 @@ import { getCurrentUser } from '../controllers/userController.js';
 
 const router = Router();
 
-
-// /**
-//  * @swagger
-//  * tags:
-//  *   name: Users
-//  *   description: User endpoints
-//  */
-
-// /**
-//  * @swagger
-//  * /me:
-//  *   get:
-//  *     summary: Get current user
-//  *     tags: [Users]
-//  *     security:
-//  *       - bearerAuth: []
-//  *     responses:
-//  *       200:
-//  *         description: Current user retrieved successfully
-//  *       401:
-//  *         description: Unauthorized
-//  */
 router.get('/me', authenticate, getCurrentUser);
 
 export default router;
